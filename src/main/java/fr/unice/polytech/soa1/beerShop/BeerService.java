fe3700a730ca927100450e4e7496fa9bfc7bfd93
TomVeniat <veniatom@gmail.com>
2015-09-28 15:51:47 +0200

    First service : get all beers

---
+++ src/main/java/fr/unice/polytech/soa1/beerShop/BeerService.java
@@ -0,0 +1,35 @@
+package fr.unice.polytech.soa1.beerShop;
+
+import org.json.JSONArray;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.ArrayList;
+import java.util.Collection;
+
+/**
+ * Created by tom on 28/09/15.
+ */
+
+@Path("/beers")
+@Produces(MediaType.APPLICATION_JSON)
+public class BeerService {
+
+    @GET
+    public Response getAvailableGenerators() {
+        Collection<String> gens = new ArrayList<String>(){{
+            this.add("Leffe");
+            this.add("Rebook");
+        }
+        };
+        JSONArray result = new JSONArray();
+        for(String g: gens) {
+            result.put(g);
+        }
+        return Response.ok().entity(result.toString(2)).build();
+    }
+
+}
--- src/main/java/fr/unice/polytech/soa1/beerShop/Main.java
+++
@@ -1,7 +0,0 @@
-package fr.unice.polytech.soa1.beerShop;
-
-/**
- * Created by tom on 28/09/15.
- */
-public class Main {
-}
