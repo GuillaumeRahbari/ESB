defc98bf3fb0937eade5c1d6a9cad7e7c4a73dac
TomVeniat <veniatom@gmail.com>
2015-09-28 17:40:14 +0200

    Add of model

--- src/main/java/fr/unice/polytech/soa1/beerShop/model/Beer.java
+++ src/main/java/fr/unice/polytech/soa1/beerShop/model/Beer.java
@@ -4,4 +4,17 @@ package fr.unice.polytech.soa1.beerShop.model;
  * Created by tom on 28/09/15.
  */
 public class Beer {
+    private String name;
+
+    public Beer(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
--- src/main/java/fr/unice/polytech/soa1/beerShop/model/User.java
+++ src/main/java/fr/unice/polytech/soa1/beerShop/model/User.java
@@ -4,4 +4,23 @@ package fr.unice.polytech.soa1.beerShop.model;
  * Created by tom on 28/09/15.
  */
 public class User {
+    private String username;
+    private String password;
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUsername() {
+
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
 }
