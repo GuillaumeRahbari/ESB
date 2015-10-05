3466e1599ce76af4cf068f4be9438a4af6c7abd8
TomVeniat <veniatom@gmail.com>
2015-10-05 16:31:19 +0200

    Wanted regression

--- src/main/java/fr/unice/polytech/soa1/beerShop/data/BeerData.java
+++ src/main/java/fr/unice/polytech/soa1/beerShop/data/BeerData.java
@@ -14,24 +14,24 @@ public class BeerData {
     private static Map<String, Beer> data = DaoUtils.readData("beerData.json", String.class, Beer.class);
 
     public static Beer get(String id){
-        DaoUtils.writeData("beerData.json", data);
+        //DaoUtils.writeData("beerData.json", data);
         return data.get(id);
     }
 
     public static void add(Beer beer){
         data.put(beer.getName(),beer);
-        DaoUtils.writeData("beerData.json", data);
+        //DaoUtils.writeData("beerData.json", data);
     }
 
     public static void update(Beer beer){
         delete(beer);
         data.put(beer.getName(), beer);
-        DaoUtils.writeData("beerData.json", data);
+        //DaoUtils.writeData("beerData.json", data);
     }
 
     public static void delete(Beer beer){
         data.remove(beer.getName());
-        DaoUtils.writeData("beerData.json", data);
+        //DaoUtils.writeData("beerData.json", data);
     }
 
     public static Map<String, Beer> getData() {
