24c01d824cd8a835b14413c4b036d5ad03415257
Rahbari Guillaume <guillaume.rahbari@gmail.com>
2015-10-06 19:51:47 +0200

    Plein de changements sur les comptes

--- src/main/java/fr/unice/polytech/soa1/beerShop/AccountService.java
+++ src/main/java/fr/unice/polytech/soa1/beerShop/AccountService.java
@@ -5,6 +5,7 @@ import fr.unice.polytech.soa1.beerShop.data.AccountData;
 import fr.unice.polytech.soa1.beerShop.data.BeerData;
 import fr.unice.polytech.soa1.beerShop.model.Account;
 import fr.unice.polytech.soa1.beerShop.model.Beer;
+import org.json.JSONArray;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
@@ -19,6 +20,31 @@ import java.util.Map;
 @Produces(MediaType.APPLICATION_JSON)
 public class AccountService {
 
+    @Path("/all")
+    @GET
+    public Response getAllAccount(@QueryParam("compte") String admin) {
+
+        //Hardcore logging
+        System.out.println("GET /account/all");
+
+        if (!AccountData.getData().containsKey(admin)){
+            return Response.status(Response.Status.NOT_FOUND).build();
+        }
+        ObjectMapper mapper = new ObjectMapper();
+
+        //Map<String,Account> users = AccountData.getData();
+        JSONArray result = new JSONArray();
+        for(Map.Entry<String, Account> entry: AccountData.getData().entrySet()) {
+
+            try {
+                result.put(mapper.readValue(entry.toString(),Account.class));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return Response.ok().entity(result.toString(2)).build();
+    }
+
     @POST
     public Response createAccount(String account){
         ObjectMapper mapper = new ObjectMapper();
@@ -26,14 +52,18 @@ public class AccountService {
         System.out.println("POST /account --- with " + account);
         try {
             Account account1 = mapper.readValue(account,Account.class);
+            System.out.println("cc");
             if (!AccountData.getData().containsKey(account1.getUsername())){
+                System.out.println("cc2");
                 AccountData.add(account1);
+                return  Response.ok().build();
             }
         } catch (IOException e) {
             e.printStackTrace();
+            return Response.status(Response.Status.NOT_FOUND).build();
         }
 
-        return  Response.ok().build();
+        return Response.status(Response.Status.NOT_FOUND).build();
     }
 
 
@@ -66,9 +96,10 @@ public class AccountService {
         for(Map.Entry<String, Account> entry: AccountData.getData().entrySet()) {
             if (entry.getValue().getUsername().equals(username) && entry.getValue().getUsername().equals(accountName)){
                 AccountData.delete(entry.getValue());
+                return  Response.ok().build();
             }
         }
-        return  Response.ok().build();
+        return Response.status(Response.Status.NOT_FOUND).build();
     }
 
     @PUT
@@ -83,13 +114,15 @@ public class AccountService {
             for(Map.Entry<String, Account> entry: AccountData.getData().entrySet()) {
                 if (entry.getValue().getUsername().equals(username) && entry.getValue().getUsername().equals(account.getUsername())){
                     AccountData.update(account);
+                    return  Response.ok().build();
                 }
             }
         } catch (IOException e) {
             e.printStackTrace();
+            return Response.status(Response.Status.NOT_FOUND).build();
         }
 
-        return  Response.ok().build();
+        return Response.status(Response.Status.NOT_FOUND).build();
     }
 
 }
